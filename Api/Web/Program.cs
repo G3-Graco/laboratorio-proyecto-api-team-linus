f62e6b0a86cdf28eba6c355086682c9c77bcb50b
linopaul1512 <139503828+linopaul1512@users.noreply.github.com>
2024-04-20 15:50:39 +0000

    Api Final2.0

--- Api/Web/Program.cs
+++ Api/Web/Program.cs
@@ -7,17 +7,16 @@ using Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 using Services.Services;
-using System.Configuration;
+using System;
+using System.IO;
 using System.Reflection;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-
-
-builder.Services.AddSwaggerGen();
 
 
 builder.Services.AddSwaggerGen(options =>
@@ -38,10 +37,10 @@ builder.Services.AddSwaggerGen(options =>
             Name = "Example License",
             Url = new Uri("https://example.com/license")
         }
-      
+
     });
 
-        // using System.Reflection;
+    // Using System.Reflection;
     var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
     options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
 });
@@ -49,32 +48,31 @@ builder.Services.AddSwaggerGen(options =>
 builder.Services.AddControllers();
 
 
-builder.Services.AddScoped(typeof(IUnitOfWork), typeof(UnitOfWork));
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped(typeof(IBaseRepository<>), typeof(BaseRepository<>));
-builder.Services.AddScoped(typeof(IArchivosRepository), typeof(ArchivosRepository));
-builder.Services.AddScoped(typeof(ICuentasRepository), typeof(CuentasRepository));
-builder.Services.AddScoped(typeof(ICuotasRepository), typeof(CuotasRepository));
-builder.Services.AddScoped(typeof(IMovimientosRepository), typeof(MovimientosRepository));
-builder.Services.AddScoped(typeof(IPrestamosRepository), typeof(PrestamosRepository));
-builder.Services.AddScoped(typeof(ISesionesRepository), typeof(SesionesRepository));
-builder.Services.AddScoped(typeof(ITasasRepository), typeof(TasasRepository));
-builder.Services.AddScoped(typeof(ITipoMovimientoRepository), typeof(TipoMovimientoRepository));
-builder.Services.AddScoped(typeof(IUsuariosRepository), typeof(UsuariosRepository));
-builder.Services.AddScoped(typeof(IArchivosPrestamosRepository), typeof(ArchivosPrestamosRepository));
+builder.Services.AddScoped<IArchivosRepository, ArchivosRepository>();
+builder.Services.AddScoped<ICuentasRepository, CuentasRepository>();
+builder.Services.AddScoped<ICuotasRepository, CuotasRepository>();
+builder.Services.AddScoped<IMovimientosRepository, MovimientosRepository>();
+builder.Services.AddScoped<IPrestamosRepository, PrestamosRepository>();
+builder.Services.AddScoped<ISesionesRepository, SesionesRepository>();
+builder.Services.AddScoped<ITasasRepository, TasasRepository>();
+builder.Services.AddScoped<ITipoMovimientoRepository, TipoMovimientoRepository>();
+builder.Services.AddScoped<IUsuariosRepository, UsuariosRepository>();
+builder.Services.AddScoped<IArchivosPrestamosRepository, ArchivosPrestamosRepository>();
 
 
 
-builder.Services.AddScoped(typeof(IUsuarioService), typeof(UsuarioService));
-builder.Services.AddScoped(typeof(IArchivosService), typeof(ArchivoService));
-builder.Services.AddScoped(typeof(ICuentasService), typeof(CuentaService));
-builder.Services.AddScoped(typeof(ICuotasService), typeof(CuotaService));
-builder.Services.AddScoped(typeof(IMovimientosService), typeof(MovimientoService));
-builder.Services.AddScoped(typeof(IPrestamosService), typeof(PrestamoService));
-builder.Services.AddScoped(typeof(ISesionService), typeof(SesionService));
-builder.Services.AddScoped(typeof(ITasasService), typeof(TasaService));
-builder.Services.AddScoped(typeof(ITipoArchivosService), typeof(TipoArchivoService));
-builder.Services.AddScoped(typeof(ITipoMovimientoService), typeof(TipoMovimientoService));
-builder.Services.AddScoped(typeof(IArchivosPrestamosRepository), typeof(ArchivosPrestamosRepository));
+builder.Services.AddScoped<IUsuarioService, UsuarioService>();
+builder.Services.AddScoped<IArchivosService, ArchivoService>();
+builder.Services.AddScoped<ICuentasService, CuentaService>();
+builder.Services.AddScoped<ICuotasService, CuotaService>();
+builder.Services.AddScoped<IMovimientosService, MovimientoService>();
+builder.Services.AddScoped<IPrestamosService, PrestamoService>();
+builder.Services.AddScoped<ISesionService, SesionService>();
+builder.Services.AddScoped<ITasasService, TasaService>();
+builder.Services.AddScoped<ITipoArchivosService, TipoArchivoService>();
+builder.Services.AddScoped<ITipoMovimientoService, TipoMovimientoService>();
 
 
 builder.Services.AddDbContext<AppDbContext>(options =>
@@ -92,18 +90,8 @@ app.MapControllers();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API Gracosoft .NET CORe"));
 }
 app.UseHttpsRedirection();
 
-
-
-
-            
-
-
-
-
-
 app.Run();
-
